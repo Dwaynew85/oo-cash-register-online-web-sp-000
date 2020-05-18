dc90c72408caf0af3faf9586a3d03b1e3bef36bc
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-18 02:42:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,5 @@
 class CashRegister
-  attr_accessor :total
+  attr_accessor :total, :title, :price
 
   def initialize
     @total = 0
@@ -8,5 +8,13 @@ class CashRegister
   def total
     @total
   end
+
+  def add_item(title, price, quantity = 1)
+    full_price = price * quantity
+    @title = title   
+    @price = full_price
+    @total + @price 
+  end
+     
 
 end
